919ea53c49539fe33c4c501d40a42565bf13b36a
Taimoor Ali <taimoor0217@gmail.com>
2020-05-28 09:42:20 +0500

    add routing

---
+++ src/components/CourseDetail copy.jsx
@@ -0,0 +1,4 @@
+import React, { Component } from 'react';
+export default function(){
+    return <h1> CourseDetails </h1>
+}
---
+++ src/components/Routing.jsx
@@ -0,0 +1,46 @@
+import React, { Suspense, lazy } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+const UpdateCourse = lazy(() => import('./UpdateCourse'));
+const CreateCourse = lazy(() => import('./CreateCourse'));
+const CourseDetail = lazy(() => import('./CourseDetail'));
+const Courses = lazy(() => import('./Courses'));
+const UserSignIn = lazy(() => import('./UserSignIn'));
+const UserSignout = lazy(() => import('./UserSignout'));
+const UserSignUp = lazy(() => import('./UserSignUp'));
+
+export default function Routing() {
+  return (
+    <Router>
+      <Suspense fallback={<div>Loading ..</div>}>
+        <Switch>
+          <Route exact path="/courses/:id/update">
+            <UpdateCourse />
+          </Route>
+          <Route exact path="/courses/create">
+            <CreateCourse />
+          </Route>
+          <Route exact path="/courses/:id">
+            <CourseDetail />
+          </Route>
+          <Route exact path="/signin">
+            <UserSignIn />
+          </Route>
+          <Route exact path="/signup">
+            <UserSignUp />
+          </Route>
+          <Route exact path="/signout">
+            <UserSignout />
+          </Route>
+          <Route exact path="/">
+            <Courses />
+          </Route>
+        </Switch>
+      </Suspense>
+    </Router>
+  );
+}
